0636dd6e96379c4f904f242c54cbd5471c0f1bd1
cetaceang <simon@dreamworld.ltd>
2025-09-02 02:16:48 +0800

    修复http

--- config/settings.py
+++ config/settings.py
@@ -238,3 +238,36 @@ else:
 # 登录URL配置
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = '/chat/'
+
+# Logging Configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
